ffd27b7cc7fb36c9bbacc904ebe2a661f918ec1c
jryuppie <jonathan.rossato@yuppie.in>
2022-10-11 19:45:09 -0300

    ajuste primeiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuDesktopComponent } from './menu/menu-desktop/menu-desktop/menu-desktop.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuDesktopComponent
   ],
   imports: [
     BrowserModule,
